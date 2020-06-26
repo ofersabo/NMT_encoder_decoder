e7744005529373c81ba389cfa6ba96fd3648056e
Ofer <>
2020-06-26 23:03:55 +0300

    get sys argvs as input

--- evaluate.py
+++ evaluate.py
@@ -4,32 +4,39 @@ import sys
 
 from allennlp.commands import main
 
-model_location = sys.argv[1]
-src_location = sys.argv[2]
-trg_location = sys.argv[3]
-output = sys.argv[4]
-
-if len(sys.argv) < 4:
-    model_location = "attention"
-    src_location = 'data/test.src'
-    trg_location = 'data/test.trg'
-    output = 'here'
+model_location = sys.argv[1] if len(sys.argv) > 1 else 'attention'
+src_location = sys.argv[2] if len(sys.argv) > 2 else 'data/src.trg'
+trg_location = sys.argv[3] if len(sys.argv) > 3 else 'data/test.trg'
+output = sys.argv[4] if len(sys.argv) > 4 else ""
 
 locations = src_location + " " + trg_location
 
 # Use overrides to train on CPU.
 # overrides = json.dumps({"trainer":{"cuda_device": 0},"iterator": {"type": "basic", "batch_size": 1},"validation_data_path": locations})
 #on_the_fly
+print(model_location)
 
-# Assemble the command into sys.argv
-sys.argv = [
-    "allennlp",  # command name, not used by main
-    "evaluate",
-    model_location,
-    locations,
-    "--include-package", "my_library",
-    "--cuda-device", 0,
-    "--output-file", output
-]
+if output != "":
+    # Assemble the command into sys.argv
+    sys.argv = [
+        "allennlp",  # command name, not used by main
+        "evaluate",
+        model_location,
+        locations,
+        "--include-package", "my_library",
+        "--cuda-device", 0,
+        "--output-file", output
+    ]
+else:
+    # Assemble the command into sys.argv
+    sys.argv = [
+        "allennlp",  # command name, not used by main
+        "evaluate",
+        model_location,
+        locations,
+        "--include-package", "my_library",
+        "--cuda-device", 0,
+    ]
+
 
 main()
