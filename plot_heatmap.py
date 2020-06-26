5324bb6a9e156e8f7bce52274aa5db9ae93afa16
Ofer <>
2020-06-26 23:03:31 +0300

    add annotation in cells

--- plot_heatmap.py
+++ plot_heatmap.py
@@ -26,7 +26,7 @@ for epoch_index,l in enumerate(lines,1):
     full_title = "attention based alignment:\n" + sub_title
     # ax.set_title(full_title)
     plt.title(full_title,fontsize=8)
-    ax = sns.heatmap(row, xticklabels=x.split(), yticklabels=y.split()[1:],cmap="Blues")
+    ax = sns.heatmap(row, xticklabels=x.split(), yticklabels=y.split()[1:],cmap="Blues",annot=True, fmt='.2f')
     # ax = sns.heatmap(row, linewidth=0.5,yticklabels=y)
     # ax.set_yticks(y[0].split()[1:])
     plt.savefig("heat_map_epoch_"+str(epoch_index)+".png")
